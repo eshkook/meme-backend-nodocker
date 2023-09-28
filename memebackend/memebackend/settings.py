bb7a229c1f3ce3e287af1b996c9483741c3f1ce5
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-28 22:58:52 +0300

    s

--- memebackend/memebackend/settings.py
+++ memebackend/memebackend/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/' ################## not like that for production
+STATIC_URL = '/static/' 
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
