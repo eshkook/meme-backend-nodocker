e8cd6ca08b1bb35e5bf0551f30d70f21efd1f939
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-28 23:15:40 +0300

    e

--- memebackend/memebackend/settings.py
+++ memebackend/memebackend/settings.py
@@ -18,13 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%e%yh-0s_!&aq=-^@&pqld#bd1r4jmct5e0@jhg$ck@5wdj-5$'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-%e%yh-0s_!&aq=-^@&pqld#bd1r4jmct5e0@jhg$ck@5wdj-5$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']#['https://v9m2jp3tgz.eu-west-1.awsapprunner.com/'] 
+ALLOWED_HOSTS = ['https://v9m2jp3tgz.eu-west-1.awsapprunner.com/'] 
 
 # Application definition
 
@@ -104,7 +104,7 @@ DATABASES = {
 # Ensure you've added boto3 and django-storages to your requirements.txt for S3 integration.
 
 # Security Settings: ?????????????????????????????????????????????????????????????????????? is it correct
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
