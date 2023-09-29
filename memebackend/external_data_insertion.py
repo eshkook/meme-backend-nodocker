e468cee7b48b64af51dfc9a434421c666793fb1c
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-29 17:55:05 +0300

    h

--- memebackend/external_data_insertion.py
+++ memebackend/external_data_insertion.py
@@ -1,6 +1,15 @@
 import psycopg2
+import pandas as pd
 
-# Database connection parameters
+# Sample DataFrame
+data = {
+    'id': [1, 2, 3],
+    'name': ['Laptop', 'Desktop', 'Tablet'],
+    'description': ['High-performance laptop', 'Gaming desktop', 'Lightweight tablet']
+}
+df = pd.DataFrame(data)
+
+# Define your connection parameters
 db_params = {
     'dbname': 'postgres',
     'user': 'postgres',
@@ -11,23 +20,22 @@ db_params = {
 
 # Establishing the connection
 conn = psycopg2.connect(**db_params)
-
 cur = conn.cursor()
 
-# Using a dictionary to store data
-data_to_insert = {
-    "name": "soccer",
-    "description": "only ucl"
-}
-
-# SQL statement
-sql = """
-INSERT INTO base_topic (name, description)
-VALUES (%(name)s, %(description)s);
+# Prepare the SQL query
+insert_sql = """
+    INSERT INTO base_topic (id, name, description)
+    VALUES (%s, %s, %s)
+    ON CONFLICT (id)
+    DO UPDATE SET
+        name = excluded.name,
+        description = excluded.description;
 """
 
-cur.execute(sql, data_to_insert)
-conn.commit()
+# Iterate over DataFrame rows and execute the SQL
+for index, row in df.iterrows():
+    cur.execute(insert_sql, (row['id'], row['name'], row['description']))
 
+conn.commit()
 cur.close()
 conn.close()
