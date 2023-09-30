296621c5c8fc364c6963e8a0c61659dd8d520748
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-30 16:24:14 +0300

    d

--- memebackend/memebackend/settings.py
+++ memebackend/memebackend/settings.py
@@ -159,5 +159,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['https://main.d1lf2gevc7dmgj.amplifyapp.com'] # specify allowed urls
+CORS_ALLOWED_ORIGINS = ['https://main.d1j24jpipbs08c.amplifyapp.com/'] # specify allowed urls
 # CORS_ALLOWED_ORIGIN_REGEXES = [] # specify allowed urls with regex
