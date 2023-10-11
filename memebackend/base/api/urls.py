8803b289a01b58ac9eaa395e629277106512235f
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-11 17:16:16 +0300

    f

--- memebackend/base/api/urls.py
+++ memebackend/base/api/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('image/', views.getImage),
-    path('posts/', views.getPosts),
+    path('posts/', views.getPosts, name='get-posts'),
+    path('posts/', views.createPost, name='create-post'),
     path('posts/<str:pk>/', views.getPost),
     path('users/', views.getUsers),
     path('users/<str:pk>/', views.getUser)
