388077baa307314f5e5e24166759b8a2c2c01b64
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-12 09:52:44 +0300

    e

--- memebackend/base/api/views.py
+++ memebackend/base/api/views.py
@@ -21,11 +21,7 @@ def find_dict_by_key_value(dict_list, target_key, target_value):
 # Load data.json once when the module is imported
 filename = os.path.join(settings.BASE_DIR, 'base', 'api', 'data.json')
 with open(filename, 'r') as file:
-    memes_list_of_dicts = json.load(file)
-
-# filename = os.path.join(settings.BASE_DIR, 'base', 'api', 'posts.json')
-# with open(filename, 'r') as file:
-#     posts_list_of_dicts = json.load(file)   
+    memes_list_of_dicts = json.load(file)  
 
 filename = os.path.join(settings.BASE_DIR, 'base', 'api', 'users.json')
 with open(filename, 'r') as file:
@@ -57,51 +53,8 @@ def getPost(request, pk):
     with open(filename, 'r') as file:
         posts_list_of_dicts = json.load(file)
 
-    post_dict = find_dict_by_key_value(posts_list_of_dicts, id, pk)
+    post_dict = find_dict_by_key_value(posts_list_of_dicts, "id", pk)
     return (Response(post_dict) if post_dict else Response("no such post"))
-
-# @api_view(['GET'])
-# def getPosts(request):
-
-#     filename = os.path.join(settings.BASE_DIR, 'base', 'api', 'posts.json')
-#     with open(filename, 'r') as file:
-#         posts_list_of_dicts = json.load(file)
-
-#     return Response(posts_list_of_dicts)   
-
-# @api_view(['POST'])
-# @parser_classes([JSONParser])  # Specify JSON parser for parsing POST request data
-# def createPost(request):
-#     new_post = request.data  # Get data from POST request
-
-#     filename = os.path.join(settings.BASE_DIR, 'base', 'api', 'posts.json')
-#     with open(filename, 'r') as file:  # Open file in read mode to get existing posts
-#         posts_list_of_dicts = json.load(file)
-
-#     # Handle missing/empty title and body
-#     title = new_post.get('title', '')
-#     body = new_post.get('body', '')
-#     if not title and not body:
-#         return Response({'error': 'Both title and body cannot be missing or empty'}, status=status.HTTP_400_BAD_REQUEST)
-
-#     # Handle missing id
-#     existing_ids = [int(post['id']) for post in posts_list_of_dicts]
-#     generated_id = str(max(existing_ids) + 1) if existing_ids else '1'
-#     post_id = new_post.get('id', generated_id)
-
-#     # Construct the new post object
-#     new_post = {
-#         'id': post_id,
-#         'title': title,
-#         'body': body
-#     }
-
-#     # Append the new post to the list and update the file
-#     posts_list_of_dicts.append(new_post)
-#     with open(filename, 'w') as file:  # Open file in write mode to update posts
-#         json.dump(posts_list_of_dicts, file, indent=4)  # Write updated data back to file
-
-#     return Response(new_post, status=status.HTTP_201_CREATED)  # Return new post with 201 Created status
 
 class PostsView(APIView):
     def get(self, request):
@@ -152,5 +105,5 @@ def getUsers(request):
 
 @api_view(['GET'])
 def getUser(request, pk):
-    user_dict = find_dict_by_key_value(users_list_of_dicts, id, pk)
+    user_dict = find_dict_by_key_value(users_list_of_dicts, "id", pk)
     return (Response(user_dict) if user_dict else Response("no such user"))
