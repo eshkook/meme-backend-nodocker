ea50851e52c42d1f5c6e58bf560a37f0cf948bc0
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-27 08:15:39 +0300

    f

--- memebackend/base/models.py
+++ memebackend/base/models.py
@@ -25,23 +25,4 @@ class Topic(models.Model):
 
     def __str__(self): 
         return self.name
-    
-# class Post(models.Model):   
-#     user= models.ForeignKey(User, on_delete=models.CASCADE) # many to one relationship. each user can send multiple messages. 
-#                                                             # when the user is deleted, the cascade makes his posts delete.
-#     title = models.CharField(max_length=200) 
-#     body = models.TextField(null=True, # it allows NUULs to be in this field in database
-#                             blank = True # it means that saving an empty form is allowed
-#                             )
-#     updated = models.DateTimeField(auto_now=True) 
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         ordering = ['-updated', '-created']
-#         db_table = 'posts_table' 
-
-#     def __str__(self): # will be accessed in html by '{{message}}'
-#         return self.body[:50] # we dont want a too long one so we truncate it   
-
-
-        
+ 
