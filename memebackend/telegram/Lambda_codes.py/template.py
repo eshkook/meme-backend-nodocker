359af20eb1523fd87550891be1a8016df4c965e0
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-02 09:06:47 +0200

    k

---
+++ memebackend/telegram/Lambda_codes.py/template.py
@@ -0,0 +1,60 @@
+import boto3
+import json
+import requests
+import traceback  
+from boto3.dynamodb.conditions import Attr
+
+# AWS DynamoDB setup
+region_name = '????????'
+table_name = '???????????????????'
+dynamodb = boto3.resource('dynamodb', region_name=region_name)
+table = dynamodb.Table(table_name)
+
+# Telegram setup
+telegram_token = '????????????????'
+api_url = f'https://api.telegram.org/bot{telegram_token}'
+sendMessage_url = f'{api_url}/sendMessage'
+edit_url = f'{api_url}/editMessageText'
+delete_url = f'{api_url}/deleteMessage'
+
+def lambda_handler(event, context):
+    try:
+        # Check if the event is from CloudWatch
+        if 'source' in event and event['source'] == 'aws.events':
+            handle_cloudwatch_event(event)
+        else:
+            handle_telegram_event(event)
+        return {"statusCode": 200}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
+        return {"statusCode": 200}
+    
+def handle_cloudwatch_event(event): 
+    # handle event logic
+
+def handle_telegram_event(event):
+    body = json.loads(event['body'])
+    chat_id = None
+    query_data = None
+    
+    if 'message' in body:
+        chat_id = body['message']['chat']['id']
+        user_message = body['message'].get('text', '')
+        message_id = body['message']['message_id']
+        user_info = body['message']['from']
+        username = user_info.get('username', '')
+        full_name = user_info.get('first_name', '') + " " + user_info.get('last_name', '')   
+
+        # handle message logic
+
+    elif 'callback_query' in body:
+        chat_id = body['callback_query']['message']['chat']['id']
+        query_data = body['callback_query']['data']
+        message_id = body['callback_query']['message']['message_id'] 
+        user_info = body['callback_query']['message']['from']
+        username = user_info.get('username', '')
+        full_name = user_info.get('first_name', '') + " " + user_info.get('last_name', '')    
+
+        # handle query logic
+          
