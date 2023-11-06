02551d438b2540cab40562f5fbb739364e0ae012
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-06 14:25:25 +0200

    d

--- memebackend/memebackend/settings.py
+++ memebackend/memebackend/settings.py
@@ -179,13 +179,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'https://main.d1j24jpipbs08c.amplifyapp.com',
-    'https://localhost:3000',
-    'https://127.0.0.1:3000',
-    'https://localhost:3001',
-    'https://127.0.0.1:3001',
-    'https://localhost:8000',
-    'https://127.0.0.1:8000',
-    'https://127.0.0.1',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:3001',
+    'http://127.0.0.1:3001',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
