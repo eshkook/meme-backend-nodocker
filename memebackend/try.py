ebb002c437932b696b3d261534bc82f9e44a798b
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-13 16:35:47 +0200

    a

--- memebackend/try.py
+++ memebackend/try.py
@@ -1,29 +1,29 @@
 # a=[271, 273, 150, 151, 152, 160, 161, 291, 292, 293, 40, 41, 180, 181, 60, 61, 190, 62, 191, 71, 72, 210, 211, 212, 90, 91, 92, 230, 232, 110, 240, 241]
 # print(sorted(a))
 
+# missing 3 level:
+# 70 Religious Architecture - v
+# 80 Ancient Ruins - v
+# 81 Historic Districts - v
+# 82 Historic Landmarks -v
+# 83 Monuments and Memorials -v
+# 111 Extreme Activities - v
+# 170 Street Food Tour - v
+# 171 Street Market Tour - v
+# 182 Beer Breweries - v
+# 192 Opera - v
+# 231 Karaoke Bars - v
+# 270 Food Tours - v
+# 272 Alcohol Tours - v
+# 290 Bus Tours - 
+
 b=[1, 2, 3, 900, 4, 6, 7, 5, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 400, 21, 24, 25, 26, 27, 28, 29, 30, 31, 800, 32, 33, 35, 34, 300, 700, 200, 1100, 100, 1000, 500]
 print(sorted(b))
 
-# missing 3 level:
-# 70 Religious Architecture
-# 80 Ancient Ruins
-# 81 Historic Districts
-# 82 Historic Landmarks
-# 83 Monuments and Memorials
-# 111 Exteme Activities
-# 170 Street Food Tour
-# 171 Street Market Tour
-# 182 Beer Breweries
-# 192 Opera
-# 231 Karaoke Bars
-# 270 Food Tours
-# 272 Alcohol Tours
-# 290 Bus Tours
-
 # missing 2 level:
-# 8 History and archeology (3 exists)
-# 20 Dance
-# 23 nightlife entertainment (3 exists)
+# 8 History and archeology (3 exists) - v
+# 20 Dance - v
+# 23 nightlife entertainment (3 exists) - v
 # 36 hockey
 # 37 american football
 # 38 Baseball
