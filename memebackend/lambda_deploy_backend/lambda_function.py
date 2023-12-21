ec2e597c4c39ae9791a1efe1c8a52117d2c83908
itai <102740785+NoamMesh@users.noreply.github.com>
2023-12-21 11:50:26 +0200

    r

--- memebackend/lambda_deploy_backend/lambda_function.py
+++ memebackend/lambda_deploy_backend/lambda_function.py
@@ -298,9 +298,12 @@ def handle_delete(event):
     headers = event.get('headers', {})
     print('headers: ', headers)
     ########################################################
+    body = json.loads(event['body'])
+    password = body['password']
     cookies = event.get('headers', {}).get('Cookie', '')
     access_token = extract_token(cookies, 'access_token')
     refresh_token = extract_token(cookies, 'refresh_token')
+    id_token = extract_token(cookies, 'id_token')
     client = boto3.client('cognito-idp')
 
     # Try to validate the access token first
@@ -309,8 +312,20 @@ def handle_delete(event):
         return {'statusCode': 401, 'body': json.dumps("An internal error occurred")}
     
     try:
+        # authentication:
         user_info = client.get_user(AccessToken=access_token)
         username = user_info['Username']
+        # validating password:
+        _ = client.admin_initiate_auth(
+            UserPoolId = 'eu-west-1_BZy97DfFY',
+            ClientId = client,
+            AuthFlow='ADMIN_NO_SRP_AUTH',
+            AuthParameters={
+                'USERNAME': username,
+                'PASSWORD': password
+            }
+        )
+        # now we can delete account:
         delete_user(client, username)
         return clear_tokens_response_200('Account Deletion Successful')
 
@@ -322,9 +337,19 @@ def handle_delete(event):
         
         try:
             # Refresh the access token using refresh token
-            # (Assuming you have a function to handle this)
             new_tokens = refresh_access_token(client, refresh_token)
             username = new_tokens['Username']
+            # validating password:
+            _ = client.admin_initiate_auth(
+                UserPoolId = 'eu-west-1_BZy97DfFY',
+                ClientId = client,
+                AuthFlow='ADMIN_NO_SRP_AUTH',
+                AuthParameters={
+                    'USERNAME': username,
+                    'PASSWORD': password
+                }
+            )
+            # now we can delete account:
             delete_user(client, username)
             return clear_tokens_response_200('Account Deletion Successful - Token Refreshed')
             
